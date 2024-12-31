dbefe5c2f1ddeb60fc85a3078da01e6955241355
sarimsaleem <sarimsaleem07@gmail.com>
2024-12-31 16:05:50 +0500

    changes

--- src/digitalMarketing/contentCreationMaintenance/keyComponentCreation/KeyComponentCreation.jsx
+++ src/digitalMarketing/contentCreationMaintenance/keyComponentCreation/KeyComponentCreation.jsx
@@ -4,7 +4,7 @@ import emailMarketinImg from "../../../assets/Competitive-Analysis-img_1.jpg";
 import { Col, Container, Row } from 'react-bootstrap';
 import emailMarketinImg1 from "../../../assets/keyComponent1.svg";
 import emailMarketinImg2 from "../../../assets/keyComponent2.svg";
-import keyComponent3 from "../../../assets/keycomponent3.svg";
+import keyComponent3 from "../../../assets/keycomponent3.png";
 import emailMarketinImg4 from "../../../assets/keyComponent4.svg";
 
 

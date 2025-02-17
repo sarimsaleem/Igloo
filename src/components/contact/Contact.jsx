da54275f31ceb4eee9c7e4af4653bf84455beed1
sarimsaleem <sarimsaleem07@gmail.com>
2025-02-17 16:02:00 +0500

    contact form improved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,129 +32,125 @@ const Contact = () => {
 
       <div style={{ paddingTop: '100px' }}>
         <section className='section-contact'>
-          <h2 style={{ margin: 0, fontSize: '36px', lineHeight: '1.5' }}>
+          <h2 className='contact-subheading'>
             LET’S WORK TOGETHER <br /> ON YOUR NEXT PROJECT
           </h2>
         </section>
       </div>
-        <Container>
-      <div className="contact-parent">
-          {/* <Row> */}
-            <div className="contact-subparent">
-              <Col md={8}>
-                <Formik
-                  initialValues={{
-                    fullName: '',
-                    email: '',
-                    mobileNumber: '',
-                    companyName: '',
-                    region: '',
-                    marketingBudget: '',
-                    hearAboutUs: '',
-                  }}
-                  validationSchema={validationSchema}
-                  onSubmit={handleSubmit}
-                >
-                  {({ handleSubmit }) => (
-                    <Form onSubmit={handleSubmit}>
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Full Name <sup>*</sup></Typography>
-                            <Field type="text" name="fullName" className="form-control" placeholder="John Doe" />
-                            <ErrorMessage name="fullName" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Email <sup>*</sup></Typography>
-                            <Field type="email" name="email" className="form-control" placeholder="Email@example.com" />
-                            <ErrorMessage name="email" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
+      <Container>
+        <div className="contact-parent">
+          <div className="contact-subparent">
+            <Formik
+              initialValues={{
+                fullName: '',
+                email: '',
+                mobileNumber: '',
+                companyName: '',
+                region: '',
+                marketingBudget: '',
+                hearAboutUs: '',
+              }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ handleSubmit }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col md={12} sm={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Full Name <sup>*</sup></Typography>
+                        <Field type="text" name="fullName" className="form-control" placeholder="John Doe" />
+                        <ErrorMessage name="fullName" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Email <sup>*</sup></Typography>
+                        <Field type="email" name="email" className="form-control" placeholder="Email@example.com" />
+                        <ErrorMessage name="email" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
 
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Mobile Number <sup>*</sup></Typography>
-                            <Field type="text" name="mobileNumber" className="form-control" placeholder="1234567890" />
-                            <ErrorMessage name="mobileNumber" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Company Name <sup>*</sup></Typography>
-                            <Field type="text" name="companyName" className="form-control" placeholder="Company XYZ" />
-                            <ErrorMessage name="companyName" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
+                  <Row>
+                    <Col md={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Mobile Number <sup>*</sup></Typography>
+                        <Field type="text" name="mobileNumber" className="form-control" placeholder="1234567890" />
+                        <ErrorMessage name="mobileNumber" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Company Name <sup>*</sup></Typography>
+                        <Field type="text" name="companyName" className="form-control" placeholder="Company XYZ" />
+                        <ErrorMessage name="companyName" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
 
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Region <sup>*</sup></Typography>
-                            <Field as="select" name="region" className="form-control">
-                              <option value=""></option>
-                              <option value="North America">North America</option>
-                              <option value="Europe">Europe</option>
-                              <option value="Asia">Asia</option>
-                              <option value="Australia">Australia</option>
-                              <option value="Africa">Africa</option>
-                              <option value="South America">South America</option>
-                            </Field>
-                            <ErrorMessage name="region" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md={12}>
-                          <div className="form-group">
-                            <Typography className="mt-3">Monthly Marketing Budget <sup>*</sup></Typography>
-                            <Field as="select" name="marketingBudget" className="form-control">
-                              <option value=""></option>
-                              <option value="below AED 10,000">below AED 10,000</option>
-                              <option value="below AED 25,000">below AED 25,000</option>
-                              <option value="below AED 50,000">below AED 50,000</option>
-                              <option value="below AED 10,000">below AED 100,000</option>
-                              <option value="Above AED 100,000">Above AED 100,000</option>
-                            </Field>
-                            <ErrorMessage name="marketingBudget" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
+                  <Row>
+                    <Col md={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Region <sup>*</sup></Typography>
+                        <Field as="select" name="region" className="form-control">
+                          <option value=""></option>
+                          <option value="North America">North America</option>
+                          <option value="Europe">Europe</option>
+                          <option value="Asia">Asia</option>
+                          <option value="Australia">Australia</option>
+                          <option value="Africa">Africa</option>
+                          <option value="South America">South America</option>
+                        </Field>
+                        <ErrorMessage name="region" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}>
+                      <div className="form-group">
+                        <Typography className="mt-3">Monthly Marketing Budget <sup>*</sup></Typography>
+                        <Field as="select" name="marketingBudget" className="form-control">
+                          <option value=""></option>
+                          <option value="below AED 10,000">below AED 10,000</option>
+                          <option value="below AED 25,000">below AED 25,000</option>
+                          <option value="below AED 50,000">below AED 50,000</option>
+                          <option value="below AED 10,000">below AED 100,000</option>
+                          <option value="Above AED 100,000">Above AED 100,000</option>
+                        </Field>
+                        <ErrorMessage name="marketingBudget" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
 
-                      <Row>
-                        <Col>
-                          <div className="form-group">
-                            <Typography className="mt-3">How Did You Hear About Us? <sup>*</sup></Typography>
-                            <Field as="select" name="hearAboutUs" className="form-control">
-                              <option value=""></option>
-                              <option value="Google">Google</option>
-                              <option value="Social Media">Social Media</option>
-                              <option value="Referral">Word Of Mouth</option>
-                            </Field>
-                            <ErrorMessage name="hearAboutUs" component="div" className="text-danger" />
-                          </div>
-                        </Col>
-                      </Row>
+                  <Row>
+                    <Col>
+                      <div className="form-group">
+                        <Typography className="mt-3">How Did You Hear About Us? <sup>*</sup></Typography>
+                        <Field as="select" name="hearAboutUs" className="form-control">
+                          <option value=""></option>
+                          <option value="Google">Google</option>
+                          <option value="Social Media">Social Media</option>
+                          <option value="Referral">Word Of Mouth</option>
+                        </Field>
+                        <ErrorMessage name="hearAboutUs" component="div" className="text-danger" />
+                      </div>
+                    </Col>
+                  </Row>
 
-                      <Button variant="primary" type="submit" className="contactUs-btn">
-                        Submit
-                      </Button>
-                    </Form>
-                  )}
-                </Formik>
-              </Col>
-            </div>
-          {/* </Row> */}
-      </div>
-        </Container>
+                  <Button variant="primary" type="submit" className="contactUs-btn">
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </Container>
     </>
   );
 };

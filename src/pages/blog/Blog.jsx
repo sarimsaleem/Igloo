eeedc4d1102f07306c27f6a31e93f37c9697b331
sarimsaleem <sarimsaleem07@gmail.com>
2025-02-26 21:10:45 +0500

    blog page first section completed

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
+import { CiHeart } from "react-icons/ci";
+import { FaRegComment } from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+
+import "./blog.css"
 
 const Blog = () => {
+
+  const blogData = [
+
+    { id: 1, src: "/public/assets/images/blog1.png", heading: "The Ultimate SEO Growth Guide for 2025", subheading: "Posted by Igloo | 19 February 2025 | SEO", description: "SEO has become more than just a priority in the UAE—it’s a fundamental aspect of long-term growth and success for any UAE business. Whether you’re running a cozy cafe in Dubai, a boutique shop in Abu Dhabi, or a service-based business in Sharjah, SEO is your secret to success.  From mobile optimization to voice search", btn: "CONTINUE READING" },
+
+    { id: 2, src: "/public/assets/images/blog1.png", heading: "The Ultimate SEO Growth Guide for 2025", subheading: "Posted by Igloo | 19 February 2025 | SEO", description: "SEO has become more than just a priority in the UAE—it’s a fundamental aspect of long-term growth and success for any UAE business. Whether you’re running a cozy cafe in Dubai, a boutique shop in Abu Dhabi, or a service-based business in Sharjah, SEO is your secret to success.  From mobile optimization to voice search", btn: "CONTINUE READING" },
+
+    { id: 3, src: "/public/assets/images/blog1.png", heading: "The Ultimate SEO Growth Guide for 2025", subheading: "Posted by Igloo | 19 February 2025 | SEO", description: "SEO has become more than just a priority in the UAE—it’s a fundamental aspect of long-term growth and success for any UAE business. Whether you’re running a cozy cafe in Dubai, a boutique shop in Abu Dhabi, or a service-based business in Sharjah, SEO is your secret to success.  From mobile optimization to voice search", btn: "CONTINUE READING" },
+
+    { id: 4, src: "/public/assets/images/blog1.png", heading: "The Ultimate SEO Growth Guide for 2025", subheading: "Posted by Igloo | 19 February 2025 | SEO", description: "SEO has become more than just a priority in the UAE—it’s a fundamental aspect of long-term growth and success for any UAE business. Whether you’re running a cozy cafe in Dubai, a boutique shop in Abu Dhabi, or a service-based business in Sharjah, SEO is your secret to success.  From mobile optimization to voice search", btn: "CONTINUE READING" },
+
+    { id: 5, src: "/public/assets/images/blog1.png", heading: "The Ultimate SEO Growth Guide for 2025", subheading: "Posted by Igloo | 19 February 2025 | SEO", description: "SEO has become more than just a priority in the UAE—it’s a fundamental aspect of long-term growth and success for any UAE business. Whether you’re running a cozy cafe in Dubai, a boutique shop in Abu Dhabi, or a service-based business in Sharjah, SEO is your secret to success.  From mobile optimization to voice search", btn: "CONTINUE READING" },
+
+  ]
+
+
+  const categories = [
+    { name: "Branding", count: 15 },
+    { name: "Content Marketing", count: 29 },
+    { name: "Designing", count: 2 },
+    { name: "Digital Marketing", count: 77 },
+    { name: "Dubai web industry", count: 4 },
+    { name: "Home", count: 12 },
+    { name: "Insights", count: 80 },
+    { name: "Lead Generation", count: 14 },
+    { name: "Logo Design", count: 2 },
+    { name: "Marketing Automation", count: 12 },
+    { name: "Mobile Marketing", count: 2 },
+    { name: "PPC", count: 16 },
+    { name: "SEO", count: 65 },
+    { name: "Social Media", count: 53 },
+    { name: "Uncategorized", count: 95 },
+    { name: "Web Design", count: 34 },
+    { name: "Website Development", count: 9 },
+  ];
+
   return (
     <>
       <Container>
@@ -11,7 +52,56 @@ const Blog = () => {
         </div>
       </Container>
 
-      
+      <div className="blog">
+        <Container>
+          <Row>
+            {blogData?.map((item, index) => (
+              <>
+                <Col md={8}>
+                  <div className="blog-item">
+                    <div className="blog-img-container">
+                      <img src={item.src} alt="" />
+                    </div>
+                    <div className="blog-text-container">
+                      <h3>{item.subheading}</h3>
+                      <h1>{item.heading}</h1>
+                      <p>{item.description}</p>
+                      <div className="icons-parent">
+                        <i><a href=""><CiHeart /><span>Like Likes</span></a></i>
+                        <i><a href=""> <FaRegComment /><span>Leave a comment</span></a></i>
+                      </div>
+                    </div>
+                    <button className='blog-btn'><a href="">{item.btn}</a></button>
+                  </div>
+                </Col>
+              </>
+            ))}
+
+            <Col md={4}>
+              <div className="blog-item2">
+                <h4 className='blog-item2-heading'>
+                  SEARCH
+                </h4>
+                <div className="blog-item2-input-parent">
+                <input className='blog-item2-input' type="text"  />
+                <IoIosSearch />
+                </div>
+                <div className="categories-container">
+                  <h4 className='blog-item2-heading'>Categories</h4>
+                  <ul className="blog-item2-list">
+                    {categories.map((category, index) => (
+                      <li key={index} className="blog-item2-item">
+                        {category.name} <span className="count">({category.count})</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
     </>
   )
 }

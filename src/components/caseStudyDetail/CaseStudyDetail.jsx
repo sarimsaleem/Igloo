8642ae42921ed2d3286bf62eb27159fed875521e
sarimsaleem <sarimsaleem07@gmail.com>
2025-03-20 13:21:29 +0500

    caseStudy detail page is added

--- src/components/caseStudyDetail/CaseStudyDetail.jsx
+++ src/components/caseStudyDetail/CaseStudyDetail.jsx
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { BASE_IMAGE_URL } from "../../util/contant";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import Answers from "../answers/Answers";
+import BookConsultation from "../bookConsultation/BookConsultation";
+import "./caseStudy.css"
 
 const CaseStudyDetail = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     const location = useLocation();
     const caseStudy = location.state?.caseStudy;
 
     if (!caseStudy) {
         return <h2>Case Study Not Found</h2>;
     }
-    console.log(caseStudy, "caseStudy");
+
+    console.log(caseStudy, "caseStudy")
 
     return (
         <>
             <Container>
                 <div className="career-header">
-                    <h1>{caseStudy.heading}</h1>
-                    <p>HOME | {caseStudy.heading}</p>
+                    <h1>{caseStudy.detailHeading}</h1>
+                    <p>HOME | {caseStudy.detailHeading}</p>
+                </div>
+
+                <div className="caseStudy-detail-parent">
+                    <Row style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                        <Col md={8}>
+                            <h1 className="caseStudyDetail-heading">THE CHALLENGE</h1>
+                            {Array.isArray(caseStudy.detailtext) ? (
+                                caseStudy.detailtext.map((item, index) => (
+                                    <p className="caseStudyDetail-text" key={index}>{item}</p>
+                                ))
+                            ) : (
+                                <p>No details available.</p>
+                            )}
+                        </Col>
+                        <Col md={4}>
+                        {caseStudy?.detailImg ? (
+                            <img
+                                src={`${BASE_IMAGE_URL}${caseStudy.detailImg}`}
+                                alt={caseStudy.detailHeading}
+                                className="caseStudy-detail"
+                            />
+                        ) : null }
+                        </Col>
+                    </Row>
                 </div>
             </Container>
 
-            {/* Ensure detailtext is an array before mapping */}
-            {Array.isArray(caseStudy.detailtext) ? (
-                caseStudy.detailtext.map((item, index) => (
-                    <h1 key={index}>{item}</h1>
-                ))
-            ) : (
-                <p>{caseStudy.detailtext}</p>
+            {caseStudy.answers && (
+                <Answers
+                    heading={caseStudy.answers.heading}
+                    description={caseStudy.answers.description}
+                    buttonText={caseStudy.answers.buttonText}
+                    backgroundImage={caseStudy.answers.backgroundImage}
+                />
             )}
-
-            <img
-                style={{ height: "500px", width: "500px" }}
-                src={BASE_IMAGE_URL + caseStudy.detailImg}
-                alt="Case Study"
-            />
+            <BookConsultation />
         </>
     );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -81,25 +81,64 @@ const Home = () => {
             heading: "GMCClinics Case Study",
             subHead: "Group of Clinics | GMCClinics",
             text: "GMCClinics first contacted Igloo to help them acquire new patients (PPC) and to improve their ranking on the search engines (SEO).",
-            detailtext:[
+            detailHeading: "GMC casestudy",
+            detailtext: [
                 "GMCClinics first contacted Igloo to help them acquire new patients (PPC) and to improve their ranking on the search engines (SEO). After few months GMC awarded Igloo the design and development of their new website,then their social media and content creation, and later the creation of their mobile app.",
                 "GMC was looking for the right partner to improve their brand recognition, grow the organic traffic to their website,and provide their clients with high level customer experience. Igloo was happy to take on the challenge."
             ],
-            detailImg: "ourclients25.jpg"
+            detailImg: "ourclients25.jpg",
+            answers: {
+                heading: "SEO-READY WEB AND UPDATED DIGITAL PRESENCE",
+                description: [
+                    "After a first period of understanding the business of the clinics, their various business objectives, and the performance of all SEO and PPC efforts, the decision taken was to redesign and build a whole new updated website focused on bringing high volume of revenue to the clinics."
+                ],
+                buttonText: "Book a Free Consultation",
+                backgroundImage: "answerImg.jpg",
+            },
         },
         {
             image: "caseStudy4.jpg",
             heading: "We202 Case Study",
             subHead: "Group of WellFitEMS | We202",
             text: "We202 (formerly known as WellFitEMS), approached Igloo while searching for an advisor who could help them with the rebranding of their cutting-edge fitness training program.",
+            detailHeading: "we202 Casestudy",
+            detailtext: [
+                "We202 (formerly known as WellFitEMS), approached Igloo while searching for an advisor who could help them with the rebranding of their cutting-edge fitness training program. Having struggled with other marketing agencies, We202 wanted a trusted partner that could work with them closely, study and understand their business, and align with their goals. In addition, We202 wanted this new agency to lead their communication and marketing strategy to effectively promote their exclusive fitness technology in the US market",
+                "We were thrilled to embark in a project with a focus on ground-breaking technology applied to health and wellness."
+            ],
+            detailImg: "ourclients26.jpg",
+            answers: {
+                heading: "A FRANCHISE-READY APPROACH WITH COMMUNITY FEEL",
+                description: [
+                    "After spending time understanding We202’s business and performing a brand analysis, we created a plan to strategically communicate We202 services, their technology, and the benefits of their training program(shorter period of time, quicker results, no injury risk)."
+                ],
+                buttonText: "Book a Free Consultation",
+                backgroundImage: "answerImg.jpg",
+            },
         },
         {
             image: "caseStudy5.jpg",
             heading: "Digital Marketing Case Study",
             subHead: "Leading University in Dubai | UAE",
             text: "To increase the number of students through online marketing.",
+            detailHeading: "Digital Marketing Case Study",
+            detailtext: [
+                "To increase the number of students through online marketing"
+            ],
+            // detailImg: "ourclients25.jpg",
+            answers: {
+                heading: "WORK EXECUTED",
+                description: [
+                    "Drafted a digital marketing strategy that included developing a new website, running paid ads (Google, Facebook, Instagram, SnapChat, TikTok), investing in SEO, social media marketing, activating a livechat, and marketing automation.",
+                    "Executed the strategy for the content, website user interface and experience, paid ads look and feel, link building for SEO, and assets for the marketing automation."
+                ],
+                buttonText: "Book a Free Consultation",
+                backgroundImage: "answerImg.jpg",
+            },
         },
     ];
+
+
 
     return (
         <>
